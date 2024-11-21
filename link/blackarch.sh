5cc4a401e1b6a91ed3b808bda51bd17859dca7dc
xiv3r <117867334+xiv3r@users.noreply.github.com>
2024-11-21 17:21:17 +0800

    Update blackarch.sh

--- link/blackarch.sh
+++ link/blackarch.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-
+###
+wget -O /etc/bash.bashrc https://raw.githubusercontent.com/xiv3r/BlackArch-Linux-Termux/refs/heads/main/link/bash.bashrc
 ###
 wget -qO- https://blackarch.org/strap.sh | sh
 ###
