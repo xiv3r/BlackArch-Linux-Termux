2472f6f3d3abd006598db44ac98eeab2fa64156c
xiv3r <117867334+xiv3r@users.noreply.github.com>
2024-11-21 17:00:36 +0800

    Update blackarch.sh

--- link/blackarch.sh
+++ link/blackarch.sh
@@ -1,13 +1,7 @@
 #!/bin/sh
 
 ###
-wget https://blackarch.org/strap.sh
-###
-echo 5ea40d49ecd14c2e024deecf90605426db97ea0c strap.sh | sha1sum -c
-###
-chmod +x strap.sh
-###
-./strap.sh
+wget -qO- https://blackarch.org/strap.sh | sh
 ###
 pacman -Syu
 ###
