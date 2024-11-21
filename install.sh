916a91c8e7507b9c212427f7556f7071c9d060cf
xiv3r <117867334+xiv3r@users.noreply.github.com>
2024-11-21 16:55:05 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,16 +11,3 @@ echo "proot-distro login archlinux" >/$PREFIX/etc/bash.bashrc
 proot-distro install archlinux
 ###
 proot-distro login archlinux
-###
-wget https://blackarch.org/strap.sh
-###
-echo 5ea40d49ecd14c2e024deecf90605426db97ea0c strap.sh | sha1sum -c
-###
-chmod +x strap.sh
-###
-./strap.sh
-###
-pacman -Syu
-###
-pacman -Syyu
-
