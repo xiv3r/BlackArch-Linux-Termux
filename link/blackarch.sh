ee4d04cae4f5c2ec6586ad35ebec1e95f9630aab
xiv3r <117867334+xiv3r@users.noreply.github.com>
2024-12-03 13:20:13 +0800

    Update blackarch.sh

--- link/blackarch.sh
+++ link/blackarch.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
+
+###
+pacman -Syyu --noconfirm
+###
+pacman-key --init
+###
+pacman-key --populate archlinuxarm
 ###
 wget -O /etc/bash.bashrc https://raw.githubusercontent.com/xiv3r/BlackArch-Linux-Termux/refs/heads/main/link/bash.bashrc
 ###
 wget -qO- https://blackarch.org/strap.sh | sh
-###
-pacman -Syu
 ###
 pacman -Syyu
 
