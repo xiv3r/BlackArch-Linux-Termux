ef9ecea557a6cab4a169b2e5f7e83aeaf2da55b9
xiv3r <117867334+xiv3r@users.noreply.github.com>
2025-02-14 18:33:03 +0800

    Update install.sh

--- link/install.sh
+++ link/install.sh
@@ -1,10 +1,78 @@
-#!/bin/bash
+#!/data/data/com.termux/files/usr/bin/bash -e
 
-###
-echo "blackarch" >> $PREFIX/etc/bash.bashrc
-###
-echo "pd sh archlinux" > $PREFIX/bin/blackarch
-###
-chmod 700 $PREFIX/bin/blackarch
-###
-pd i archlinux
+# Install required packages
+apt install proot proot-distro bsdtar axel neofetch -y
+clear
+
+# Variables
+FS="blackarch"
+
+# Forward the value of FS to NM
+NM="$FS"
+
+# Capitalize the first letter of NM
+NM=$(echo "$NM" | awk '{print toupper(substr($0,1,1)) substr($0,2)}')
+
+# Display the values of FS and NM
+echo "$FS" > /dev/null
+echo "$NM" > /dev/null
+
+# Display neofetch with custom ASCII art
+neofetch --ascii_distro "${NM}" -L
+
+# Create necessary directories
+mkdir -p "$PREFIX/var/lib/proot-distro/installed-rootfs"
+
+# Change directory
+cd "$PREFIX/var/lib/proot-distro/installed-rootfs/"
+
+# Download the rootfs using axel
+axel -a -o "${FS}.tar.xz" "https://github.com/xiv3r/Termux-Pentesting-Distro/releases/download/rootfs/${FS}.tar.xz"
+
+# Extract the rootfs
+echo " "
+echo "[*] Extracting ${NM} Linux Rootfs...!!!"
+proot --link2symlink bsdtar -xpJf "${FS}.tar.xz" 2>/dev/null
+
+# Create a proot-distro configuration file for the Kali minimal rootfs
+cat > "$PREFIX/etc/proot-distro/${FS}.sh" << EOF
+DISTRO_NAME="${NM} Linux"
+TARBALL_URL['aarch64']="https://github.com/xiv3r/Termux-Pentesting-Distro/releases/download/rootfs/${FS}.tar.xz"
+TARBALL_SHA256['aarch64']="$(sha256sum "${FS}.tar.xz" | awk '{print $1}')"
+EOF
+
+# Create a shortcut command to launch the distro
+cat > "$PREFIX/bin/${FS}" << EOF
+#!/data/data/com.termux/files/usr/bin/bash
+proot-distro login ${FS}
+EOF
+chmod 700 "$PREFIX/bin/${FS}"
+
+# Add start-up login notification
+cat >> $PREFIX/etc/bash.bashrc << EOF
+${FS}
+EOF
+
+# Add support for uninstallation
+cat > $PREFIX/bin/uninstall-${FS} << EOF
+#!/data/data/com.termux/files/usr/bin/bash
+proot-distro remove ${FS}
+sed -i 's/echo "Login ${NM} Linux: ${FS}"//g' $PREFIX/etc/bash.bashrc
+rm -rf $PREFIX/bin/uninstall-${FS}
+rm -rf $PREFIX/bin/${FS}
+rm -rf $PREFIX/var/lib/proot-distro/dlcache/${FS}.tar.xz
+EOF
+chmod 700 "$PREFIX/bin/uninstall-${FS}"
+
+# Move tarball to cache
+mkdir -p "$PREFIX/var/lib/proot-distro/dlcache"
+mv "${FS}.tar.xz" "$PREFIX/var/lib/proot-distro/dlcache"
+
+# Display final instructions
+echo -e '\e[1;93m'
+cat << EOF
+Successful Installation!'
+
+To login ${NM} Linux, Type: ${FS}
+EOF
+echo " "
